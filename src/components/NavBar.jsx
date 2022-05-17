604c8610e2b60f3189d34198c245d62f5437ade7
EnriqueSFlor <enriquesilvaflor@gmail.com>
2022-05-16 21:58:22 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import style from "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="menu">
+      <p>Teste menu</p>
+      <p>Teste menu</p>
+      <p>Teste menu</p>
+    </div>
+  );
+}
